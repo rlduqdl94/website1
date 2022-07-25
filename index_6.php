875280feea33b28c20af4a980c3f430c75f659c7
dlrlduq <dlrlduq94@gmail.com>
2022-07-25 10:42:13 +0900

    0725

--- index_6.php
+++ index_6.php
@@ -3286,6 +3286,29 @@ License: For each use you must have a valid license purchased only from above li
 						<!--end::Container-->
 					</div>
 					<!--end::Header tablet and mobile-->
+
+					<div class="py-6 py-lg-0 bg-warning" >
+						<div class="header-container">
+							<!--begin::Page title-->
+							<div class="page-title d-flex flex-column align-items-start justify-content-center flex-wrap me-lg-20 py-3 py-lg-0 me-3">
+								<!--begin::Heading-->
+								<div class="d-flex align-items-center flex-shrink-0">
+									<div class="me-3 py-3 py-lg-0">
+										<a href="#" class="btn btn-danger btn-sm rounded-0"><i class="fas fa-flag fs-4 me-2"></i> 공지사항</a>
+									</div>
+
+									<div class="content">
+										공지사항 내용
+									</div>
+								</div>
+								<!--end::Heading-->
+
+
+							</div>
+						</div>
+
+					</div>
+
 					<!--begin::Header-->
 					<div id="kt_header" class="header py-6 py-lg-0" data-kt-sticky="true" data-kt-sticky-name="header" data-kt-sticky-offset="{lg: '300px'}">
 						<!--begin::Container-->
@@ -4867,18 +4890,17 @@ License: For each use you must have a valid license purchased only from above li
 						<!--end::Container-->
 					</div>
 					<!--end::Header-->
+
 					<!--begin::Content-->
 					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
 						<!--begin::Container-->
 						<div class="container-xxl" id="kt_content_container">
 							<!--begin::Row-->
 							<div class="row g-5 g-xl-8">
-								<!--begin::Col-->
-								<div class="col-xl-4">
-									<!--begin::Misc Widget 1-->
+								<div class="col-xl-12">
 									<div class="row mb-5 mb-xl-8 g-5 g-xl-8">
 										<!--begin::Col-->
-										<div class="col-6">
+										<div class="col-3">
 											<!--begin::Card-->
 											<div class="card card-stretch">
 												<!--begin::Link-->
@@ -4900,7 +4922,7 @@ License: For each use you must have a valid license purchased only from above li
 										</div>
 										<!--end::Col-->
 										<!--begin::Col-->
-										<div class="col-6">
+										<div class="col-3">
 											<!--begin::Card-->
 											<div class="card card-stretch">
 												<!--begin::Link-->
@@ -4922,7 +4944,7 @@ License: For each use you must have a valid license purchased only from above li
 										<!--end::Col-->
 
 										<!--begin::Col-->
-										<div class="col-6">
+										<div class="col-3">
 											<!--begin::Card-->
 											<div class="card card-stretch">
 												<!--begin::Link-->
@@ -4943,7 +4965,7 @@ License: For each use you must have a valid license purchased only from above li
 										</div>
 										<!--end::Col-->
 										<!--begin::Col-->
-										<div class="col-6">
+										<div class="col-3">
 											<!--begin::Card-->
 											<div class="card card-stretch">
 												<!--begin::Link-->
@@ -4964,6 +4986,11 @@ License: For each use you must have a valid license purchased only from above li
 										</div>
 										<!--end::Col-->
 									</div>
+								</div>
+								<!--begin::Col-->
+								<div class="col-xl-4">
+									<!--begin::Misc Widget 1-->
+
 									<!--end::Misc Widget 1-->
 									<!--begin::List Widget 5-->
 									<div class="card mb-5 mb-xl-8">
@@ -5124,108 +5151,6 @@ License: For each use you must have a valid license purchased only from above li
 								<!--begin::Col-->
 								<div class="col-xl-8 ps-xl-12">
 
-									<div class="card mb-xl-8">
-										<!--begin::Header-->
-										<div class="card-header border-0 pt-5">
-											<h3 class="card-title align-items-start flex-column">
-												<span class="card-label fw-bold fs-3 mb-1">공지사항</span>
-												<!-- <span class="text-muted mt-1 fw-semibold fs-7">More than 400 new products</span> -->
-											</h3>
-
-										</div>
-
-
-										<div class="card-body py-3">
-
-													<div class="table-responsive">
-														<!--begin::Table-->
-														<table class="table table-row-dashed table-row-gray-200 align-middle gs-0 gy-4" id="example2">
-															<!--begin::Table head-->
-															<thead>
-																<tr class="border-0">
-																	<th class="p-0 w-50px"></th>
-																	<th class="p-0 min-w-150px"></th>
-																	<th class="p-0 min-w-140px"></th>
-																	<th class="p-0 min-w-110px"></th>
-																	<th class="p-0 min-w-50px"></th>
-																</tr>
-															</thead>
-
-															<tbody>
-
-
-																<tr>
-																	<td>
-																		<div class="symbol symbol-45px me-2">
-																			<span class="symbol-label">
-																				<img src="assets/media/svg/brand-logos/plurk.svg" class="h-50 align-self-center" alt="" />
-																			</span>
-																		</div>
-																	</td>
-																	<td>
-																		<a href="#" class="text-dark fw-bold text-hover-primary mb-1 fs-6">Brad Simmons</a>
-																		<span class="text-muted fw-semibold d-block">Movie Creator</span>
-																	</td>
-																	<td class="text-end text-muted fw-bold">React, HTML</td>
-																	<td class="text-end">
-																		<span class="badge badge-light-success">Approved</span>
-																	</td>
-																	<td class="text-end">
-																		<a href="#" class="btn btn-sm btn-icon btn-bg-light btn-active-color-primary">
-																			<!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
-																			<span class="svg-icon svg-icon-2">
-																				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																					<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor" />
-																					<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor" />
-																				</svg>
-																			</span>
-																			<!--end::Svg Icon-->
-																		</a>
-																	</td>
-																</tr>
-
-
-
-																<tr>
-																	<td>
-																		<div class="symbol symbol-45px me-2">
-																			<span class="symbol-label">
-																				<img src="assets/media/svg/brand-logos/plurk.svg" class="h-50 align-self-center" alt="" />
-																			</span>
-																		</div>
-																	</td>
-																	<td>
-																		<a href="#" class="text-dark fw-bold text-hover-primary mb-1 fs-6">Brad Simmons</a>
-																		<span class="text-muted fw-semibold d-block">Movie Creator</span>
-																	</td>
-																	<td class="text-end text-muted fw-bold">React, HTML</td>
-																	<td class="text-end">
-																		<span class="badge badge-light-success">Approved</span>
-																	</td>
-																	<td class="text-end">
-																		<a href="#" class="btn btn-sm btn-icon btn-bg-light btn-active-color-primary">
-																			<!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
-																			<span class="svg-icon svg-icon-2">
-																				<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																					<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="currentColor" />
-																					<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="currentColor" />
-																				</svg>
-																			</span>
-																			<!--end::Svg Icon-->
-																		</a>
-																	</td>
-																</tr>
-															</tbody>
-															<!--end::Table body-->
-														</table>
-													</div>
-													<!--end::Table-->
-												</div>
-
-
-
-	<br>
-									</div>
 
 
 
